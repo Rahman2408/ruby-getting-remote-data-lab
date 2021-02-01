83df062f9fd6b97ba5481ab3610827c3ca692806
Abdelrahman Elsirafy <rahman2408@gmail.com>
2021-02-01 10:00:43 -0500

    commit tree cdcda24e5e2a39af938dce97ec03b2c61341d4c7

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+class GetRequester
+
+attr_accessor :uri, :response, :url
+    
+def initialize(url)
+        @url = "#{url}"
+        @uri = URI.parse(@url)
+    end 
+
+    def get_response_body
+        @response = Net::HTTP.get_response(@uri)
+        @response.body
+    end
+
+    def parse_json
+    parsed = JSON.parse(self.get_response_body)
+    parsed.tap do |key, val|
+        "#{key}"["#{val}"]
+        end
+    end
+  
+end
